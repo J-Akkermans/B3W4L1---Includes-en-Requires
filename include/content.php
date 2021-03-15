e666da5c096b02c14a3091a2d3f10f7aa0e04d0d
Jens Akkermans <99044668@mydavinci.nl>
2021-03-15 10:59:22 +0100

    Includen af,

---
+++ include/content.php
@@ -0,0 +1,4 @@
+<?php 
+$content = '<img src="image0.jpg" alt="foto">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam sint aliquam voluptatem, laboriosam aspernatur quidem laudantium suscipit debitis, fuga a aut tempora? Mollitia facilis sint ad deserunt vel placeat libero. </p>' 
+?>
